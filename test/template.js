2f7b21cc74deed9af4e12656e057fda44e083ed2
tu4mo <tu4mo@users.noreply.github.com>
2016-11-13 16:25:43 +0200

    Add test for template.getTemplateFiles

---
+++ test/template.js
@@ -0,0 +1,24 @@
+const assert = require('assert')
+const path = require('path')
+const templatesPath = path.resolve(__dirname, 'templates')
+
+process.env.TEG_HOME = templatesPath
+
+const template = require('../lib/template')
+
+describe('template', () => {
+  describe(`getTemplateFiles('react-class')`, () => {
+    it('should return 3 template files (index.css, index.js and test.txt)', done => {
+      template.getTemplateFiles('react-class')
+      .then(files => {
+        const templatePath = path.resolve(templatesPath, 'react-class')
+        assert.equal(files.length, 3)
+        assert.equal(files.includes(path.resolve(templatePath, 'index.css')), true)
+        assert.equal(files.includes(path.resolve(templatePath, 'index.js')), true)
+        assert.equal(files.includes(path.resolve(templatePath, 'test.txt')), true)
+        done()
+      })
+      .catch(err => done(err))
+    })
+  })
+})
---
+++ test/templates/react-class/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MyComponent extends Component {
+  render () {
+    return (
+      <div className="my-component">
+
+      </div>
+    )
+  }
+}
+
+export default MyComponent
