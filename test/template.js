dfa072843fe290b38c4546d5a3a44b2c6e7ef843
tu4mo <tu4mo@users.noreply.github.com>
2016-11-15 19:06:51 +0200

    Use test-template for testing

--- test/template.js
+++ test/template.js
@@ -1,17 +1,17 @@
 const assert = require('assert')
 const path = require('path')
-const templatesPath = path.resolve(__dirname, '..', 'templates')
+const templatesPath = path.resolve(__dirname)
 
 process.env.TEG_HOME = templatesPath
 
 const template = require('../lib/template')
 
 describe('template', () => {
-  describe(`getTemplateFiles('react-class')`, () => {
+  describe(`getTemplateFiles`, () => {
     it('should return 3 template files (index.css, index.js and test.txt)', done => {
-      template.getTemplateFiles('react-class')
+      template.getTemplateFiles('test-template')
       .then(files => {
-        const templatePath = path.resolve(templatesPath, 'react-class')
+        const templatePath = path.resolve(templatesPath, 'test-template')
         assert.equal(files.length, 3)
         assert.equal(files.includes(path.resolve(templatePath, 'index.css')), true)
         assert.equal(files.includes(path.resolve(templatePath, 'index.js')), true)
---
+++ test/test-template/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class {{file}} extends Component {
+  render () {
+    return (
+      <div className="{{file}}">
+
+      </div>
+    )
+  }
+}
+
+export default {{file}}
