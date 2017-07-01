047bea7fe33bdff0ac91200b475f50f3e31b05ae
tu4mo <tu4mo@users.noreply.github.com>
2017-07-01 11:22:07 +0300

    Add react-class-scss and react-const-scss templates

---
+++ templates/react-class-scss/_index/_index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './{{file}}.scss'
+
+class {{file}} extends Component {
+  static propTypes = {
+
+  }
+
+  render () {
+    return (
+      <div className="{{file|paramCase}}">
+
+      </div>
+    )
+  }
+}
+
+export default {{file}}
---
+++ templates/react-const-scss/_index/_index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './{{file}}.scss'
+
+const {{file}} = props => {
+  return (
+    <div className="{{file|paramCase}}">
+
+    </div>
+  )
+}
+
+{{file}}.propTypes = {
+
+}
+
+export default {{file}}
